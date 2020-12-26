2b866c55d1cd798630c8ea5d20c94548a475f3a5
Merojiddin <merojiddin_1998@mail.ru>
2020-12-26 22:02:45 +0800

    header added

--- src/Components/@common/header.jsx
+++ src/Components/@common/header.jsx
@@ -1,31 +1,114 @@
 import React from 'react'
-import { Container, makeStyles } from "@material-ui/core";
-import SvgIcon from '@material-ui/core/SvgIcon';
+import { makeStyles } from "@material-ui/core";
 
-import Logo from '../../Assets/logo_white.svg'
 
 
 const Header = () => {
   const classes = useStyles()
   return (
     <div className={classes.wrapper}>
-      <Container className={classes.container}>
-        <SvgIcon src={Logo} fontSize="large"/>
-      </Container>
+      <div className={classes.container}>
+        <div className={classes.logoContainer}>
+          <button className={`${classes.jobLogo} ${classes.btn}` }>Jobs</button>
+        </div>
+        <div className={classes.navBarContainer}>
+          <ul className={classes.navBar}>
+            <li className={classes.navBarItem}><a href="#" className={classes.navBarItemLink}>About</a></li>
+            <li className={classes.navBarItem}><a href="#" className={classes.navBarItemLink}>Jobs</a></li>
+            <li className={classes.navBarItem}><a href="#" className={classes.navBarItemLink}>Companies</a></li>
+            <li className={classes.navBarItem}><a href="#" className={classes.navBarItemLink}>Contacts</a></li>
+          </ul>
+        </div>
+        <div className={classes.signUpInContainer}>
+          <button className={`${classes.navBarSignIn} ${classes.btn}`}>Sign In</button>
+          <button className={`${classes.navBarSignUp} ${classes.btn}`}>Sign Up</button>
+        </div>
+      </div>
     </div>
   )
 }
 
+//Just using makeStyles for making it easier to read
 const useStyles = makeStyles(() => ({
   wrapper: {
     width: '100%',
     backgroundColor: '#2c2c2c',
+    display: 'flex',
   },
   container: {
     maxWidth: 1140,
     width: '100%',
-    margin: '0px auto'
-  }
+    margin: '0px auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '15px 5%'
+  },
+  logoContainer: {
+    height: 60,
+    display: 'flex',
+    float: 'left'
+  },
+  btn: {
+    color: '#D3D3D3',
+    fontSize: 24,
+    cursor:'pointer',
+    borderRadius: '10px',
+    width: 100,
+    backgroundColor: '#4c4c4c',
+    borderStyle: 'none',
+    height: 35,
+    alignSelf: 'center',
+    '&:focus': {
+      outline: 'none'
+    },
+    '&:hover': {
+      backgroundColor: 'rgba(76,76,76, 0.4)'
+    }
+    
+  },
+  jobLogo: {
+    fontSize: 32,
+    border: 'none',
+    cursor:'pointer',
+    backgroundColor: 'Transparent',
+    '&:hover': {
+      backgroundColor: 'Transparent', 
+      color: 'grey'
+    }
+  },
+  navBarContainer: {
+    float: 'right',
+    display: 'flex',
+  },
+  navBarItem: {
+    padding: '0px 20px',
+    display: 'inline-block',
+    textDecoration: 'none',
+  },
+  navBarItemLink: {
+    color: '#D3D3D3',
+    fontSize: 24,
+    '&:hover': {
+      color: 'grey',
+    }
+  },
+  signUpInContainer: {
+    display: 'flex',
+
+  },
+  navBarSignUp: {
+    backgroundColor:  '#3174EE',
+    marginLeft: 10,
+    height: 55,
+    width: 130,
+    '&:hover': {
+      backgroundColor: 'rgba(49,116,238, 0.4)'
+    }
+  },
+
+
+  
 }))
 
 export default Header;
--- src/Components/test/jobs.js
+++ src/Components/test/jobs.js
@@ -1,22 +1,121 @@
 import React, { useState } from 'react'
+import { makeStyles } from "@material-ui/core";
 
+const Job = {
+  Title: 'Developer',
+  CompanyName: "Developed Company",
+  Description: `Rent out your parking space or your garage and earn $4,000+ / year!
+  We are looking for side hustlers and passive income lovers to join Neighbor as independent hosts
+  Neighbor connects hosts with unused space to renters in need of storage. Earn extra cash by renting out your empty space today! Neighbor is a great compliment to your dog walking, door dashing, airport driving, rv sharing, car sharing, or other side gig. Neighbor is a self-storage and parking community that allows`,
+
+}
 
 
 
 const Api = {
- 
+ base: 'https://www.zippia.com/api/jobs/'
 }
 
 const Jobs = () => {
-  const [query, setQuery] = useState('')
+  const classes = useStyles();
+  //const [query, setQuery] = useState('');
  
+  const responses = fetch(Api.base)
+  .then(response => {
+    console.log(response)
+    return response.json()
+  })
+  .then(data => console.log("Response:"+data));
   
   return (
-    <div>
-      Look at the console
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <h1 className={classes.mainTitle}>Jobs For Me</h1>
+
+        <div className={classes.searchBar}> 
+          <ul className={classes.searchBarContainer}>
+            <li className={classes.searchBarItems}>
+              Company Name
+            </li>
+            <li className={classes.searchBarItems}>
+              Time
+            </li>
+          </ul>
+        </div>
+
+        <div className={classes.jobs}>
+          <div className={classes.jobContainer}>
+            <h3 className={classes.jobTitle}>{Job.Title}</h3>
+            <h4 className={classes.jobCompany}>{Job.CompanyName}</h4>
+            <p className={classes.jobDescription}>
+              {Job.Description.length < 250 
+              ? Job.Description
+              : Job.Description.slice(0, 250) + ' ...' }
+            </p>
+            <button className={classes.jobApplyBtn}>Apply</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100%'
+  },
+  container: {
+    maxWidth: 1140,
+    width: '100%',
+    margin: 'auto',
+    padding: '20px'
+  },
+  mainTitle: {
+    fontSize: 36,
+    lineHeight: '48px',
+
+  },
+  searchBar: {
+    margin: '20px 0px'
+  },
+  searchBarContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    
+  },
+
+  jobs:{
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+
+  },
+  jobContainer: {
+    width: 240,
+    height: 262,
+    padding: '15px', 
+    border: 'solid 2px black'   
+  },
+  jobTitle: {
+    fontWeight: 'bold',
+    fontSize: 28,
+    lineHeight: '36px',
+
+
+  },
+  jobCompany: {
+    fontWeight: 300,
+    lineHeight: '24px',
+    fontSize: 20,
+
+  },
+  jobDescription: {
+    height: 190,
+    fontSize: 16,
+  },
+  jobApplyBtn: {
+    float: 'right',
+  }
+}))
 
 export default Jobs
